c83cbc010d5712e68f970010c0f4880682850cd7
gotypingtest <gotypingtest@gmail.com>
2026-07-19 18:01:05 +0530

    Delete src directory

--- src/app/exam-test/exam-typing/page.tsx
+++
@@ -1,5 +0,0 @@
-import ExamTypingTest from "../../../components/typingpracticepage/Typingtest";
-
-export default function Typingtest() {
-  return <ExamTypingTest />;
-}
--- src/app/exam-test/typing-result/page.tsx
+++
@@ -1,5 +0,0 @@
-import TypingtestResult from "../../../components/typingpracticepage/TypingtestResult";
-
-export default function TypingResult() {
-  return <TypingtestResult />;
-}
